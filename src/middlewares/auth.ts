0a9201661473a160a7a31a10143b7a92654c854b
Alisson Cavalcanti Galvão <alisson.galvao@amcom.com.br>
2023-08-12 00:13:00 -0300

    Ajuste path middlewares

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,6 @@
 
 import { Request, Response, NextFunction } from "express";
-import { User } from "../src/models/User";
+import { User } from "../models/User";
 
 export const Auth = {
     private: async(req: Request, res: Response, next: NextFunction) => {
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,7 +4,7 @@ import * as ApiController from '../controllers/apiController';
 /* The line `import { Auth } from '../../middlewares/auth';` is importing the `Auth` middleware from
 the `auth.ts` file located in the `middlewares` directory. This middleware is likely used to
 authenticate and authorize requests before they reach the `list` endpoint. */
-import { Auth } from '../../middlewares/auth';
+import { Auth } from '../middlewares/auth';
 
 const router = Router();
 
